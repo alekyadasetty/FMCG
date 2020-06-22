69c749f4cd180a894bb93bed9f8875e717a19608
alekyadasetty <66626321+alekyadasetty@users.noreply.github.com>
2020-06-22 20:18:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,9 @@ Created on Wed Apr  1 14:53:49 2020
 """
 
 import numpy as np
-from flask import Flask, request, jsonify, render_template, current_app
+from flask import Flask, request, jsonify, render_template
 import pickle
-app=Flask(__name__)
-         
-   
-
+app=Flask(__name__,template_folder='template',static_folder='static')
 
 
 model = pickle.load(open('model.pkl','rb'))
@@ -20,20 +17,20 @@ model = pickle.load(open('model.pkl','rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST','GET'])
+@app.route('/predict',methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features=[int(x) for x in request.form.values()]
+    
+   
+    int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 4)
 
     return render_template('index.html', prediction_text='Achieved Sales Value is  $ {}'.format(output))
-
-
 
 
 if __name__ == "__main__":
